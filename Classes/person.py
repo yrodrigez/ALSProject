7ee5355af53e5ddd42ca322881bcd2d9e5378db4
yago <y_rodrigez@hotmail.com>
2016-04-18 15:36:07 +0200

    Primer commit

--- Classes/person.py
+++ Classes/person.py
@@ -5,6 +5,6 @@ from google.appengine.ext import ndb
 
 class Person(ndb.Model):
         name = ndb.StringProperty(required=True)
-        surname = ndb.StringProperty(required=True)
+        surname = ndb.StringProperty(required=False)
         phone = ndb.IntegerProperty()
 
